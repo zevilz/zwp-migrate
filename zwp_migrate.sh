d7da4b444425dfa036bb7c86eac165ad7fa11c05
zEvilz <zevilz13@yandex.ru>
2023-08-21 12:24:04 +0500

    Updated version

--- zwp_migrate.sh
+++ zwp_migrate.sh
@@ -3,7 +3,7 @@
 # URL: https://github.com/zevilz/zwp-migrate
 # Author: Alexandr "zEvilz" Emshanov
 # License: MIT
-# Version: 1.2.1
+# Version: 1.3.0
 
 # shellcheck disable=SC2046
 # shellcheck disable=SC2089
